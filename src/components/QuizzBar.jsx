06186e73686879c39107fef195bca346ed582acf
Mewlyscin <Tear-Lyons7@live.fr>
2023-11-23 20:45:40 +0100

    adding proptypes and button image

--- src/components/QuizzBar.jsx
+++ src/components/QuizzBar.jsx
@@ -1,38 +1,45 @@
-import questions from "../questions.json"
-import { useModal } from "../ModalContext";
 import { useState } from "react";
 import Popup from "reactjs-popup";
 import PopUp from "./popup";
+// import PropTypes from "prop-types";
 
-
-
-function QuizzBar({quizz, id, choix, question, reponse}  ) {
-
+function QuizzBar({ quizz }) {
   const [count, setCount] = useState(1);
 
   const handleClick = () => {
-    setCount(count+1)
+    setCount(count + 1);
+  };
 
-  }
-
-  return (
-      quizz.map((q)=>(
-    <div key={q.id}  className="flex flex-col">
-      <Popup trigger= {<button onClick= {handleClick} > BOUTON </button>} >
-        {close => (
+  return quizz.map((q) => (
+    <div key={q.id} className="flex flex-col">
+      <Popup
+        trigger={
+          <button onClick={handleClick}>
+            <img src={q.image} alt="image" />
+          </button>
+        }
+      >
+        {(close) => (
           <div>
-            <PopUp/>
+            <PopUp />
             <a title="clase" onClick={close}>
               close
             </a>
           </div>
         )}
-      </Popup >
-
+      </Popup>
     </div>
-
-      ) )
-  )
+  ));
 }
+
+// QuizzBar.propTypes = {
+//   quizz: PropTypes.array({
+//     image: PropTypes.string,
+//     id: PropTypes.string,
+//     choix: PropTypes.array.string,
+//     question: PropTypes.string,
+//     reponse: PropTypes.string,
+//   }).isRequired,
+// };
 
 export default QuizzBar;
