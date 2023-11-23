eb561dd0133fb0c4725d877117812de206a1413c
Mewlyscin <Tear-Lyons7@live.fr>
2023-11-23 21:11:33 +0100

    adding button fix img

--- src/components/QuizzBar.jsx
+++ src/components/QuizzBar.jsx
@@ -1,22 +1,50 @@
 import { useState } from "react";
 import Popup from "reactjs-popup";
 import PopUp from "./popup";
-// import PropTypes from "prop-types";
+import rodo from "../assets/Rodolf.png";
+import lutin from "../assets/Lutins.png";
+import gift from "../assets/gift.png";
 
-function QuizzBar({ quizz }) {
+import PropTypes from "prop-types";
+
+function QuizzBar({ question }) {
   const [count, setCount] = useState(1);
 
   const handleClick = () => {
     setCount(count + 1);
   };
 
-  return quizz.map((q) => (
-    <div key={q.id} className="flex flex-col">
+  return (
+    <div className="flex flex-col">
       <Popup
         trigger={
-          <button onClick={handleClick}>
-            <img src={q.image} alt="image" />
-          </button>
+          <div>
+            <button
+              className="icon"
+              type="button"
+              onClick={() => handleClick()}
+            >
+              <img src={rodo} alt="icon" />
+              {question}
+            </button>
+
+            <button
+              className="icon"
+              type="button"
+              onClick={() => handleClick()}
+            >
+              <img src={lutin} alt="icon" />
+              {question}
+            </button>
+            <button
+              className="icon"
+              type="button"
+              onClick={() => handleClick()}
+            >
+              <img src={gift} alt="icon" />
+              {question}
+            </button>
+          </div>
         }
       >
         {(close) => (
@@ -29,17 +57,11 @@ function QuizzBar({ quizz }) {
         )}
       </Popup>
     </div>
-  ));
+  );
 }
 
-// QuizzBar.propTypes = {
-//   quizz: PropTypes.array({
-//     image: PropTypes.string,
-//     id: PropTypes.string,
-//     choix: PropTypes.array.string,
-//     question: PropTypes.string,
-//     reponse: PropTypes.string,
-//   }).isRequired,
-// };
+QuizzBar.propTypes = {
+  question: PropTypes.string.isRequired,
+};
 
 export default QuizzBar;
